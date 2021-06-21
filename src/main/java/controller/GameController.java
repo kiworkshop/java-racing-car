c72d7a7114ff1e2f1eaf36f9e7b2be537bcba937
Beenie93 <seongbeen93@naver.com>
2021-06-21 12:37:50 +0900

    feat: 이름에 ','가 포함되었는지를 체크하는 기능 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -3,12 +3,22 @@ package controller;
 import view.InputView;
 
 public class GameController {
+    private static final String DEFAULT_DELIMITER = ",";
+
     private final InputView inputView;
 
-    public GameController(InputView inputView) {
+    public GameController(final InputView inputView) {
         this.inputView = inputView;
     }
 
     public void start() {
+        String names = inputView.inputNames();
+        validateDelimiter(names);
+    }
+
+    public void validateDelimiter(String names) {
+        if (!names.contains(DEFAULT_DELIMITER)) {
+            throw new RuntimeException("\",\"가 포함되어야 합니다.");
+        }
     }
 }
