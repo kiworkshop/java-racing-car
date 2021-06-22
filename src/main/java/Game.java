a18848681dc38dd6df5645d39136093c0a597982
eunii <eoeh04@gmail.com>
2021-06-22 21:33:15 +0900

    Game 구현

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-    public static void main(String[] args) {
+    static OutputView outputView = new OutputView();
+    static InputView inputView = new InputView();
+    static CarNameParser carnameParser = new CarNameParser();
+    static MoveCountParser moveCountParser = new MoveCountParser();
 
+    public static void start() {
+        outputView.askCarNames();
+        List<String> carNames = carnameParser.splitCarNames(inputView.getInput());
+
+        outputView.askRepeatCount();
+        int repeatCount = moveCountParser.parseCount(inputView.getInput());
+
+        outputView.printRaceStart();
+
+        List<Car> cars = new ArrayList<>();
+        for (String carname : carNames) {
+            cars.add(new Car(carname));
+        }
+        Race race = new Race(cars);
+
+        for (int i = 0; i < repeatCount; i++) {
+            race.runOnce();
+            outputView.printOneRoundResult(race.getCars());
+        }
+        List<Car> winners = race.getWinners();
+        outputView.printWinners(winners);
     }
 
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,4 +9,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class GameTest {
 
 
+    @Test
+    void 시도할_횟수가_음수일때_런타임에러() {
+        // given
+
+        // when
+
+        // then
+    }
 }
