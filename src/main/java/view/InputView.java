ad911341c7cc44e579a92b56d498c92a14ef18d6
Beenie93 <seongbeen93@naver.com>
2021-06-23 15:28:10 +0900

    style: 입력 문구 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,12 +8,15 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public String inputNames() {
-        String inputNames = scanner.next();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
+        String inputNames = scanner.nextLine();
         StringUtils.isBlank(inputNames);
         return inputNames.trim();
     }
 
     public int inputRounds() {
+        System.out.println();
+        System.out.println("시도할 회수는 몇회인가요?");
         int inputRounds = scanner.nextInt();
         return inputRounds;
     }
