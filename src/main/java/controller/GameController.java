270b81e11906e631fdc2b7bc48656664885b210b
Beenie93 <seongbeen93@naver.com>
2021-06-29 17:37:03 +0900

    feat: 자동차 이름, 시도횟수 입력받는 InputView 클래스 구현

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import view.InputView;
+
+public class GameController {
+
+    public void run() {
+        String carNames = InputView.inputCarNames();
+        String tryNumber = InputView.inputTryNumber();
+    }
+}
