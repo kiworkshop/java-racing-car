d91c3a4ee7bff0c92c068919b94ad0e1881ad46d
Chloe <page.prologue@gmail.com>
2021-07-01 01:15:18 +0900

    문자열 메시지 String format 으로 변경

--- src/main/java/game/racingcar/CarRacing.java
+++ src/main/java/game/racingcar/CarRacing.java
@@ -11,13 +11,13 @@ public class CarRacing {
         String inputCarName = getUserInput("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
         List<String> carNames = service.getCarNames(inputCarName);
         while (!service.isValidCarNames(carNames)) {
-            String message = "자동차 이름을 다시 입력해 주세요. (공백 또는 " + CarRacingService.MAX_CAR_NAME_COUNT + "자 초과)";
+            String message = String.format("자동차 이름을 다시 입력해 주세요. (공백 또는 %s 자 초과)", CarRacingService.MAX_CAR_NAME_COUNT);
             carNames = service.getCarNames(getUserInput(message));
         }
 
         String inputGameCount = getUserInput("시도할 회수는 몇회인가요?");
         while (!service.isValidGameCount(inputGameCount)) {
-            String message = "게임 횟수는 " + CarRacingService.MAX_GAME_COUNT + "회를 넘을 수 없습니다.";
+            String message = String.format("게임 횟수는 %s 회를 넘을 수 없습니다.", CarRacingService.MAX_GAME_COUNT);
             inputGameCount = getUserInput(message);
         }
         int gameCount = Integer.parseInt(inputGameCount);
