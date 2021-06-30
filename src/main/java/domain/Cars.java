5e7c2a1c179c95767461f307dd0802bfe70aecdb
Beenie93 <seongbeen93@naver.com>
2021-06-30 23:00:15 +0900

    feat: 모든 자동차의 대수가 2대 미만일 경우 예외가 발생하는 테스트 코드 및 기능 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
+    private static final int MIN_CARS_SIZE = 2;
+
     private final List<Car> cars;
 
     public Cars(String... carNames) {
@@ -14,7 +16,14 @@ public class Cars {
     }
 
     public Cars(List<Car> cars) {
+        validateSize(cars);
         this.cars = cars;
+    }
+
+    private void validateSize(List<Car> cars) {
+        if (cars.size() < MIN_CARS_SIZE) {
+            throw new IllegalArgumentException("자동차의 대수는 최소 1대 이상이여야 합니다.");
+        }
     }
 
     public int size() {
--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarsTest {
 
@@ -15,5 +16,14 @@ public class CarsTest {
 
         //then
         assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차 수가 2개 미만일 경우, 예외가 발생한다.")
+    void validateSize() {
+        //given //when //then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Cars("pobi"))
+                .withMessage("자동차의 대수는 최소 1대 이상이여야 합니다.");
     }
 }
