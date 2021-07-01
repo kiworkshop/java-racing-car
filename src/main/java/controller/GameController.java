d157d5ab2b383792400e085bc6f0ab8b741b6d01
Beenie93 <seongbeen93@naver.com>
2021-07-01 19:44:29 +0900

    feat: 자동차들의 현재 위치 및 우승자 이름 출력하는 OutputView 구현 및 적용

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -4,16 +4,30 @@ import domain.GameSystem;
 import domain.Names;
 import domain.TryNumber;
 import view.InputView;
+import view.OutputView;
 
 public class GameController {
 
     public void run() {
+        GameSystem gameSystem = setUp();
+        playGame(gameSystem);
+    }
+
+    private GameSystem setUp() {
         Names carNames = new Names(InputView.inputCarNames());
         TryNumber tryNumber = new TryNumber(InputView.inputTryNumber());
         GameSystem gameSystem = new GameSystem(carNames, tryNumber);
+        return gameSystem;
+    }
+
+    private void playGame(GameSystem gameSystem) {
+        OutputView.printGameFlow();
 
         while (gameSystem.on()) {
             gameSystem.playEachRound();
+            OutputView.printEachRound(gameSystem.cars());
         }
+
+        OutputView.printWinners(gameSystem.winners());
     }
 }
